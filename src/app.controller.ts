a80ad095bacebf6eb7141b14d5b6f33625d813c9
cc <1442122744@qq.com>
2023-12-04 14:05:53 +0800

    feat: 短链服务

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Query,
+  Redirect,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ShortLongMapService } from './short-long-map.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Inject()
+  private shortLongMapService: ShortLongMapService;
+
+  @Get('short_url')
+  async generateShortUrl(@Query('url') url: string): Promise<string> {
+    return this.shortLongMapService.generate(url);
+  }
+
+  @Get(':code')
+  @Redirect()
+  async jump(@Param('code') code: string) {
+    const longUrl = await this.shortLongMapService.getLongUrl(code);
+    if (!longUrl) {
+      throw new BadRequestException('短链不存在');
+    }
+    return {
+      url: longUrl,
+      statusCode: 302,
+    };
   }
 }
